c6fa1ac25590362d5fb45833ed2cd09ff8afdcea
Naoya Noguchi <naoya.work0213@gmail.com>
2025-05-19 21:40:45 +0900

    midleware実装

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,14 @@
+// この middleware 関数は Next.js の ミドルウェア機能（ページ遷移やAPIリクエスト前に実行される処理）
+// を使って、Supabaseのログイン情報（セッション）を確認する仕組み
+
+// 	•	ログインしてるか確認したり
+// 	•	認証情報をサーバー側で使えるようにしたり
+
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
 import type { Database } from "@/lib/database.types";
-
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
