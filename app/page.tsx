bb725f21d46812e8cf4f7587940757b64b88ae29
Naoya Noguchi <naoya.work0213@gmail.com>
2025-05-20 22:12:42 +0900

    headerのナビゲーション導入

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { Database } from "@/lib/database.types";
 
-export default function Home() {
+// メインページ
+export default async function Home() {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  });
+
+  // セッションに取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
-    <div>
-      <div>HOME</div>
+    <div className="text-center text-x1">
+      {session ? <div>ログイン済</div> : <div>未ログイン</div>}
     </div>
   );
 }
